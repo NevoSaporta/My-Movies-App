7e93fa9da28120f93b375710a60b9f28d76b1a37
Nevo Saporta <nevosap@gmail.com>
2019-12-18 23:48:15 +0200

    Added pictures to git & added hebrew support

--- app/src/main/java/com/android/academy/MainActivity.kt
+++ app/src/main/java/com/android/academy/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        movie_trailer_btn.setOnClickListener { val webpage: Uri = Uri.parse(getString( R.string.avengers_iw_url))
+        movie_trailer_btn.setOnClickListener { val webpage: Uri = Uri.parse(getString( R.string.avengers_url))
             val intent = Intent(Intent.ACTION_VIEW, webpage)
             if (intent.resolveActivity(packageManager) != null) {
                 startActivity(intent)
