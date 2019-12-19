66eef77b77a869102d7ebf5ab39f75720bc24ced
nevo <nevosap@gmail.com>
2019-12-19 16:23:58 +0200

    Added Movie activity , layout and item layout

---
+++ app/src/main/java/com/android/academy/MoviesActivity.kt
@@ -0,0 +1,10 @@
+package com.android.academy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoviesActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
