1f48bc47252176f0e815ecf6a9e802e96a44aa21
NevoSaporta <nevosap@gmail.com>
2020-01-31 19:44:49 +0200

    Created AppDatabase

---
+++ app/src/main/java/com/android/academy/database/AppDatabase.kt
@@ -0,0 +1,32 @@
+package com.android.academy.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.android.academy.model.MovieModel
+
+@Database(entities = [MovieModel::class],version = 1)
+abstract class AppDatabase:RoomDatabase() {
+    companion object{
+        private const val DATABASE_NAME = "movies"
+        private var INSTANCE: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+            return INSTANCE
+        }
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+
+    }
+}
