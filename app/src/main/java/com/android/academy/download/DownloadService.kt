5edd71dedbe8bc1f7c2f0bc2821f29784f45a040
NevoSaporta <nevosap@gmail.com>
2020-02-10 10:36:12 +0200

    Added notifications

--- app/src/main/java/com/android/academy/download/DownloadService.kt
+++ app/src/main/java/com/android/academy/download/DownloadService.kt
@@ -1,14 +1,20 @@
 package com.android.academy.download
 
-import android.app.Activity
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.android.academy.R
 
 class DownloadService:Service() {
 
     companion object {
         const val URL: String = "URL"
+        const val ONGOING_NOTIFICATION_ID: Int = 14000605
+        private const val CHANNEL_DEFAULT_IMPORTANCE = "Channel"
 
         fun startService(activity: Activity, url: String) {
             val intent = Intent(activity, DownloadService::class.java)
@@ -22,6 +28,46 @@ class DownloadService:Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground()
+        var url = intent!!.getStringExtra(URL)
         return START_STICKY
     }
+
+    private fun startForeground() {
+        createNotificationChannel()
+        createNotification(0)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //SK
+            val name = getString(R.string.channel_name) // The user-visible name of the channel.
+            val description = getString(R.string.channel_description) // The user-visible description of the channel.
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(CHANNEL_DEFAULT_IMPORTANCE, name, importance)
+            mChannel.description = description // Configure the notification channel.
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.RED
+            mChannel.enableVibration(true)
+            mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+
+    private fun createNotification(progress: Int): Notification {
+        val notificationIntent = Intent(this, DownloadActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        val progressStr = getString(R.string.notification_message, progress)
+
+        return NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+            .setContentTitle(getText(R.string.notification_title))
+            .setContentText(progressStr)
+            .setSmallIcon(android.R.drawable.stat_sys_upload)
+            .setProgress(100, progress, false)
+            .setContentIntent(pendingIntent)
+            .build()
+    }
+
 }
